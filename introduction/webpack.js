5f328e42e2a65c488a20d56c375d9b8844e4072e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-06-26 06:02:05 +0100

    Add comments to webpack

--- introduction/webpack.js
+++ introduction/webpack.js
@@ -1,11 +1,15 @@
 // Initialize a basic config object : {}
 
 var config = {};
+
+// Main entry point
 config.entry = './main.js';
+
+// Main output directory and file
 config.output = {
   path: __dirname + '/dist',
   filename: 'bundle.js'
 };
 
-// Lets export the object
+// Lets export the config
 module.exports = config;
